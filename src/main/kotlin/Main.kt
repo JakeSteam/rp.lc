30a67a281ce0dc0260423eae3eac26ae33709248
Jake Lee <jake.lee@photobox.com>
2023-03-05 22:44:05 +0000

    Completely untested basic structure

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,32 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    val inputDir = "/input/"
+    val outputDir = "/output/"
+
+    val inputPath = locateImage(inputDir) ?: return
+    val inputData = loadImage(inputPath)
+    val outputData = convertImage(inputData)
+    saveOutput(outputDir, inputPath.name, outputData)
+}
+
+fun locateImage(inputDir: String): File? {
+    return File(inputDir).listFiles()?.firstOrNull()
+}
+
+fun loadImage(inputFile: File): BufferedImage {
+    return ImageIO.read(inputFile)
+}
+
+fun convertImage(inputData: BufferedImage): BufferedImage {
+    val topLeft = inputData.getRGB(0, 0)
+    inputData.setRGB(0, 0, topLeft + 100)
+    return inputData
+}
+
+fun saveOutput(outputDir: String, outputName: String, outputData: BufferedImage) {
+    val outputFile = File(outputDir, outputName)
+    ImageIO.write(outputData, "png", outputFile)
 }
